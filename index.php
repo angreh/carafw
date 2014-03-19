5357fac0800fe93927d4b9ac7e9e7b1ddc42beed
Angreh <Angreh@192.168.25.167>
2014-03-19 13:19:15 -0300

    Funcionando pelo menos uma página, só ajustes

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <?php
 
+// Importa las clases que se utilizarán
 require 'clases/Ayudante.php';
 $ayudante = new Ayudante();
 
-$usuario = $ayudante->Usuarios()->lista();
 
+// Define cuáles son los códigos HTML que se utilizarán
 $tpl = $ayudante->Template('layouts/base.html');
 $tpl->addFile('CONTENIDO', 'paginas/index.html');
+
+
+// Código PHP
+$usuario = $ayudante->Usuarios()->lista();
 $tpl->USUARIO = $usuario[0];
+
+
+// Muestra el contenido final
 $tpl->show();
