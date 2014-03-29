322dacdc3a09040d9ad7c2fa0106c3faacd7a751
Sisardo <Sisardo@Snow>
2014-03-29 15:07:45 +0100

    conexion a base de datos malo

--- admin/login.php
+++ admin/login.php
@@ -11,13 +11,38 @@ $tpl = $ayudante->Template('layouts/admin/layoutLogin.html');
 // pega esse arquivo e joga dentor de contenido
 $tpl->addFile('CONTENIDO', 'paginas/admin/login.html');
 
+if (isset($_GET["desconectado"]) && $_GET["desconectado"] == 1) {
+    session_name("session");
+    session_start();
+    session_unset();
+    session_destroy();
+    header("location: admin/login.php");
+};
+if (!empty($_POST)) {
+      
+    if (preg_match("/^[A-Z][a-z]{0,9}$/", $_POST["login"]) && preg_match("/^[A-Z][a-z]{0,5}[0-9]{1,3}$/", $_POST["password"])) {
+     
+        $conBBDD = new mysqli("localhost", "root", "123456", "tblfestivales");
+        if ($conBBDD->connect_errno) {
+            if ($tpl->exists("AVISO"))
+                $tpl->AVISO = 'no se a podido conectar a la BBDD intetalo mas tarde';
+        };
+        $rcsDato = $conBBDD->query("SELECT login from admnistrador where login" . $_POST["login"] . "and password=" . $_POST["password"] . ";");
+        exit(var_dump($rcsDato));
+        if ($rcsDato->num_rows == 1) {
+            session_name("session");
+            session_start();
+            header("location:admin/index.php");
+        };
+        $conBBDD->close();
+    };
+    
+}
+$tpl->ACTION = $_SERVER["PHP_SELF"];
 
 // Código PHP
 //$usuario = $ayudante->Usuarios()->lista();
 //$tpl->USUARIO = $usuario[0];
-
-
 // Muestra el contenido final
 $tpl->show();
-
 ?>
