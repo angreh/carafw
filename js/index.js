66b247f6efeb9e766268d4a87174f310251f724d
Sisardo <Sisardo@Snow>
2014-04-23 21:35:02 +0200

    no he conseguido poner los selects en usuarios

--- js/index.js
+++ js/index.js
@@ -1,10 +1,8 @@
 window.onload=function(){
     document.getElementById("idMenuAgenda").onclick=function(){
-        document.location='agenda.php';
+        document.location='../agenda.php';
     };
     document.getElementById("idMenuHome").onclick=function(){
-      document.location="index.php" ; 
+      document.location="../index.php" ; 
     };
 };
-
-
--- registro/usuario.php
+++ registro/usuario.php
@@ -1,7 +1,7 @@
 <?php
 
 // Importa las clases que se utilizarán
-require 'clases/Ayudante.php';
+require '../clases/Ayudante.php';
 $ayudante = new Ayudante();
 
 
@@ -11,31 +11,32 @@ $tpl = $ayudante->Template('layouts/layoutBase.html');
 // pega esse arquivo e joga dentor de contenido
 $tpl->addFile('CONTENIDO', 'paginas/usuario.html');
 
-$conBBDD = new mysqli("localhost", "usuario", "123456", "municipios");
+$conBBDD = new mysqli("localhost", "root", "123456", "tblfestivales");
 if ($conBBDD->connect_errno) {
     if ($tpl->exists("AVISO"))
         $tpl->AVISO = 'no se a podido conectar a la BBDD intetalo mas tarde';
 }
-$rcsDatos = $conBBDD->query("select id, comunidad from comunidades order by comunidad;");
+$rcsDatos = $conBBDD->query("select id, comunidad from tblfestivales.comunidades order by comunidad;");
 if ($rcsDatos->num_rows == 0) {
-    exit;
+    if ($tpl->exists("AVISO"))
+        $tpl->AVISO = 'esta mal echa la conexcion a la base de datos';
 }
 
-
+//$tpl->COMUNIDAD = 'while ($oComunidad = $rcsDatos->fetch_object()){ echo "<option value=" . $oComunidad->id . ">" . utf8_encode($oComunidad->comunidad) . "</option>"}';
 while ($oComunidad = $rcsDatos->fetch_object()) {
     echo "<option value=" . $oComunidad->id . ">" . utf8_encode($oComunidad->comunidad) . "</option>";
 }
 
-$lstProvincia = $conBBDD->query("SELECT provincias.id,provincias.provincia FROM provincias
+$lstProvincia = $conBBDD->query("SELECT provincias.id,provincias.provincia FROM tblfestivales.provincias
                         inner join comunidades on provincias.comunidad_id=comunidades.id where provincias.comunidad_id=1 order by provincia");
 while ($oProvinci = $lstProvincia->fetch_object()) {
     echo "<option value=" . $oProvinci->id . ">" . utf8_encode($oProvinci->provincia) . "</option>";
 }
 
-$consLocali = $conBBDD->query("select municipios.id,municipios.municipio from municipios
-                        inner join provincias on municipios.provincia_id=provincias.id where provincia_id=4 order by municipios.municipio");
+$consLocali = $conBBDD->query("select municipios.id,municipios.municipios from tblfestivales.municipios
+                        inner join provincias on municipios.id_provincia=provincias.id where id_provincia=4 order by municipios.municipios");
 while ($oLocalidad = $consLocali->fetch_object()) {
-    echo "<option value=" . $oLocalidad->id . ">" . utf8_encode($oLocalidad->municipio) . "</option>";
+    echo "<option value=" . $oLocalidad->id . ">" . utf8_encode($oLocalidad->municipios) . "</option>";
 }
  echo "<img src=imagenes/lupa.png alt='confirmacion' />" ;
 // Código PHP
